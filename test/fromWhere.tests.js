998527a4a4ff9882bff0a4613bb19dd123e4afe1
lilliantyiso@gmail.com <lilliantyiso@gmail.com>
2021-04-14 11:15:28 +0200

    changes made

--- test/fromWhere.tests.js
+++ test/fromWhere.tests.js
@@ -6,7 +6,7 @@ describe('fromWhere function' , function(){
             assert.equal('Paarl', fromWhere('CJ'));
             });
             it('Should return location the registration is from', function(){
-                assert.equal('Some other place!', fromWhere('CY'));
+                assert.equal('Some other place!', fromWhere('Cz'));
                 });
      
     });
